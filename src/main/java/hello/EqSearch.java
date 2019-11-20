6012b6a535dc388ee154f363efb0cf3af978b68f
Abel Semma <asemma@cstl-17.cs.ucsb.edu>
2019-11-19 23:15:27 -0800

    Added Eq search and web controller

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class EqSearch{
+    private int distance;
+    private int minmag;
+
+    EqSearch(){}
+
+    public int getDistance (){
+        return this.distance;
+    }
+
+    public int getMinmag (){
+        return this.minmag;
+    }
+
+    public void setDistance (int dis){
+        this.distance=dis;
+    }
+
+    public void setMinmag (int m){
+        this.minmag=m;
+    }
+}
