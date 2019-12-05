b86beb3683e69326af157a7ef5ce32fa706600a1
asemmaa <asemma@umail.ucsb.edu>
2019-12-04 23:18:21 -0500

    was not pushing from local so manually updated

--- src/main/java/earthquakes/EqSearch.java
+++ src/main/java/earthquakes/EqSearch.java
@@ -3,6 +3,10 @@ package earthquakes.searches;
 public class EqSearch {
    private int distance;
    private int minmag;
+   private double lat;
+   private double lon;
+   private String location;
+
 
    public EqSearch () {
    }
@@ -22,4 +26,28 @@ public class EqSearch {
    public int getMinmag () {
       return minmag;
    }
+   public double getLat() {
+        return this.lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return this.lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
 }	
