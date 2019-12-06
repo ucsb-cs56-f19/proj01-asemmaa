f42d277079b06bb2bb5f5dd4cac5fd25bc8746c2
Abel Semma <asemma@cstl-17.cs.ucsb.edu>
2019-12-05 20:48:47 -0800

    uid is now stored to location

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.Map;
 import java.util.HashMap;
 
@@ -21,19 +22,21 @@ import earthquakes.osm.Place;
 import earthquakes.entities.Location;
 import earthquakes.repositories.LocationRepository;
 
+import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
 import java.util.List;
-
-import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
 @Controller
 public class LocationsController {
 
     private LocationRepository locationRepository;
+
+    // @Autowired
+    // private ClientRegistrationRepository clientRegistrationRepository;
+
     @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-    public LocationsController (LocationRepository locationRepository) {
-        this.locationRepository = locationRepository;   
+    public LocationsController(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
     }
 
     @GetMapping("/locations/search")
@@ -45,28 +48,39 @@ public class LocationsController {
     @GetMapping("/locations/results")
     public String getLocationsResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
             LocSearch locSearch) {
-                LocationQueryService e = new LocationQueryService();
+        LocationQueryService e = new LocationQueryService();
         model.addAttribute("locSearch", locSearch);
         String json = e.getJSON(locSearch.getLocation());
-        model.addAttribute("json",json);
+        model.addAttribute("json", json);
         List<Place> placeCollection = Place.listFromJson(json);
-        model.addAttribute("placeCollection",placeCollection);
-
+        model.addAttribute("placeCollection", placeCollection);
         return "locations/results";
     }
+
     @GetMapping("/locations")
-    public String index(Model model) {
-        Iterable<Location> locations = locationRepository.findAll();
+    public String index(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        String uid = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("id").toString();
+        Iterable<Location> locations = locationRepository.findByUid(uid);
         model.addAttribute("locations", locations);
         return "locations/index";
     }
 
-    @PostMapping("/locations/add")
-    public String add(Location location, Model model) {
-      locationRepository.save(location);
-      model.addAttribute("locations", locationRepository.findAll());
-      return "locations/index";
+    @GetMapping("/locations/admin")
+    public String admin(Model model) {
+        Iterable<Location> locations = locationRepository.findAll();
+        model.addAttribute("locations", locations);
+        return "locations/admin";
     }
+
+    @PostMapping("/locations/add")
+    public String add(Location location, Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        String uid = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("id").toString();
+        location.setUid(uid);
+        locationRepository.save(location);
+        model.addAttribute("locations", locationRepository.findByUid(uid));
+        return "locations/index";
+    }
+
     @DeleteMapping("/locations/delete/{id}")
     public String delete(@PathVariable("id") long id, Model model) {
         Location location = locationRepository.findById(id)
@@ -75,6 +89,5 @@ public class LocationsController {
         model.addAttribute("locations", locationRepository.findAll());
         return "locations/index";
     }
-
 
 }
