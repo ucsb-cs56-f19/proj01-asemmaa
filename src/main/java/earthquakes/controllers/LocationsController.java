59fb8cd049ff581b048559b6d414b6b162298ccd
Abel Semma <asemma@cstl-17.cs.ucsb.edu>
2019-12-05 20:30:36 -0800

    add link to EqSearch

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -7,8 +7,10 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -58,12 +60,20 @@ public class LocationsController {
         model.addAttribute("locations", locations);
         return "locations/index";
     }
-    
+
     @PostMapping("/locations/add")
     public String add(Location location, Model model) {
       locationRepository.save(location);
       model.addAttribute("locations", locationRepository.findAll());
       return "locations/index";
+    }
+    @DeleteMapping("/locations/delete/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+        Location location = locationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+        locationRepository.delete(location);
+        model.addAttribute("locations", locationRepository.findAll());
+        return "locations/index";
     }
 
 
